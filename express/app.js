ab836e35a26443423da022ed96a9f60bfaec5edf
Artur01011 <9362561@gmail.com>
2023-07-12 23:08:19 +0400

    Sync and Async functions samples in nodejs

--- express/app.js
+++ express/app.js
@@ -1,8 +1,28 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
+// app.get('/', function (req, res) {
+//     res.send('Hello World')
+// })
+
+// app.listen(3000)
+
+const fs = require('fs')
+const fileName = "target.txt"
+
+const errHandler = (err) => console.log(err)
+const dataHandler = (data) => console.log(data.toString())
+
+
+const data = fs.readFileSync(fileName)
+console.log(data.toString())
+
+fs.readFile(fileName, (err, data)=> {
+    if(err) {
+        errHandler(err)
+    } else {
+        dataHandler(data)
+    }
 })
 
-app.listen(3000)
+console.log("Node js async programming...")
